4cea97a0a725fc60b749901716505d58bbe114b5
bunaller <132920416+bunaller@users.noreply.github.com>
2023-06-07 14:55:28 -0400

    Created the Python file

---
+++ PyTicTacToe.py
@@ -0,0 +1,15 @@
+def tictactoe(values):
+	print("\n\n")
+	print("\t     |     |")
+	print("\t  {}  |  {}  |  {}".format(values[0], values[1], values[2]))
+	print('\t_____|_____|_____')
+
+	print("\t     |     |")
+	print("\t  {}  |  {}  |  {}".format(values[3], values[4], values[5]))
+	print('\t_____|_____|_____')
+
+	print("\t     |     |")
+
+	print("\t  {}  |  {}  |  {}".format(values[6], values[7], values[8]))
+	print("\t     |     |")
+	print("\n")
